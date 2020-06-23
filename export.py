a282f135bbd1865f616a6c3b81c0816b77aa9450
S.Naito <na.shota.1139@gmail.com>
2020-06-23 11:55:09 +0900

    add models

--- export.py
+++ export.py
@@ -9,7 +9,7 @@ def create_model():
     model = keras.Sequential([
         keras.layers.Flatten(input_shape=(28, 28)),
         keras.layers.Dense(128, activation='relu'),
-        keras.layers.Dense(10, activation='softmax')
+        keras.layers.Dense(2, activation='softmax')
     ])
     model.compile(optimizer='adam',
                   loss='sparse_categorical_crossentropy',
